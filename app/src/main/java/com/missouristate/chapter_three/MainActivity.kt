1cca8744f0b88c5d64b58b6d2370733b58b1ffd4
aPalermooo <ajp2s@missouristate.edu>
2024-09-27 18:38:27 -0500

    activity 1 finished

--- app/src/main/java/com/missouristate/chapter_three/MainActivity.kt
+++ app/src/main/java/com/missouristate/chapter_three/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.missouristate.chapter_three
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.missouristate.chapter_three.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         //BINDING SET UP
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.d(TAG, "onCreate(Bundle) called")
 
 
         //https://developer.android.com/develop/ui/views/notifications/snackbar/action
@@ -67,4 +74,30 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
+
+
+
+    /// LOGGING (Assignment 3.1)
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+
+
 }
