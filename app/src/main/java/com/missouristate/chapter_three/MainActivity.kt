544b9e2f66d65715031696d565837c03f12c7055
aPalermooo <ajp2s@missouristate.edu>
2024-09-27 19:57:54 -0500

    Complete Exercise 3

--- app/src/main/java/com/missouristate/chapter_three/MainActivity.kt
+++ app/src/main/java/com/missouristate/chapter_three/MainActivity.kt
@@ -28,7 +28,9 @@ class MainActivity : AppCompatActivity() {
 
     //lateinit allows initializing a not-null property outside of a constructor
 
-
+    // initialize counters (3.3)
+    private var answersCorrect : Double = 0.0
+    private var answersSubmitted : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,6 +70,7 @@ class MainActivity : AppCompatActivity() {
         binding.falseButton.isEnabled = true
     }
 
+
     private fun checkAnswer (userAnswer:Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
@@ -76,11 +79,32 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect
         }
 
+        // Increment Counters for test grader (3.2)
+        if (userAnswer == correctAnswer) {
+            answersCorrect++
+        }
+        answersSubmitted++
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
 
-        // Disable buttons (3.2)
+        // Disable buttons (3.3)
         binding.trueButton.isEnabled = false
         binding.falseButton.isEnabled = false
+
+
+        //debug - test grader (3.3)
+        val dBugMsg = "CA = $answersCorrect | SA = $answersSubmitted"
+        Log.d("GRADER", dBugMsg)
+
+        //test grader toast (3.3)
+        if (answersSubmitted == questionBank.size.toDouble()) {
+            val answersCorrectPercentage : Double = (answersCorrect / answersSubmitted) * 100
+            val testResultsResId = String.format("%.2f",answersCorrectPercentage) + "%"
+            Toast.makeText(this, testResultsResId, Toast.LENGTH_SHORT).show()
+
+            answersCorrect = 0.0
+            answersSubmitted = 0.0
+        }
     }
 
 
